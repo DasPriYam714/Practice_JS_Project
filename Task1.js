fec695da046043070caa79817b5e567373faebe8
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-06 13:40:32 +0600

    practice task

---
+++ Task1.js
@@ -0,0 +1,19 @@
+var amount = 1000;
+var apple = 150;
+var orange = 200;
+console.log(" I need 3 kg apple");
+
+var applePrice = apple * 3;
+
+console.log("Total apple price: " + applePrice);
+
+console.log("I need two kg Orange");
+
+var orangePrice = orange * 2;
+
+console.log("Total orange price: " + orangePrice);
+
+var priceReturned = amount - (applePrice + orangePrice);
+
+console.log("Seller will return me: " + priceReturned);
+
---
+++ practice.js
@@ -0,0 +1,29 @@
+//  practice variable
+
+var price1 = 38;
+console.log(price1);
+
+var price2= 38.55;
+console.log(typeof price2);//using of typeof
+
+var total = price1 + price2;
+
+console.log(total.toFixed(1)); //use of toFixed() function
+var item = parseFloat(total.toFixed(1));//use of perseFlot() function
+console.log(item);
+
+
+console.log(40/3); //give the total answer
+console.log (40%3);//give only the reminder of the total answer
+
+
+var mainPrice=45;
+newPrice= mainPrice + price1;
+console.log(newPrice);
+discountedPrice= newPrice - price1;
+console.log(discountedPrice);
+
+
+
+
+
