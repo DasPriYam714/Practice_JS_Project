bb710bf85fa4469b46dc32dc09b363ee54d3092f
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-06 14:28:26 +0600

    Task2

---
+++ Task2.js
@@ -0,0 +1,10 @@
+var bangla = 89;
+var english= 80;
+var math = 99;
+var biology = 91;
+
+console.log("Bangla: " + bangla + ", English: " + english + ", Math: " + math + ", Biology: " +biology );
+
+var avg = (bangla + english + math+ biology) / 4;
+
+console.log("Average of four subjects:" + avg);
