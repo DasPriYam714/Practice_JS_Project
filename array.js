d12d91b14efd177672dbc3695230d4f37a7be2c2
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-16 12:52:50 +0600

    array

---
+++ array.js
@@ -0,0 +1,21 @@
+//check array length and idex.
+const number= [2,5,6,2,9,4,1,9]
+console.log(number);
+// console.log(number.length);
+// console.log(number[7]);
+///////////////////////////
+//check push pop.
+
+number.push(100,500 ,600);
+console.log(number);
+number.pop();
+console.log(number.length);
+console.log(number);
+
+//using shift and unshift.
+
+number.shift();
+console.log(number);
+
+number.unshift(33);
+console.log(number);
